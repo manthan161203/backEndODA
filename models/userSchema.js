8f735e346c0adfe03084e81d07481019946228e8
manthan161203 <manthanlp3204@gmail.com>
2023-12-13 17:22:07 +0530

    useSchema working

---
+++ models/userSchema.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['patient', 'doctor', 'therapist', 'admin', 'superAdmin'],
+        required: true
+    },
+    otp: {
+        code: {
+            type: String
+        },
+        expiryTime: {
+            type: Time
+        }
+    },
+    image: {
+        type: String,
+        required: false
+    },
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
