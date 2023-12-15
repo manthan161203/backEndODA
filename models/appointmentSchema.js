7d3620e74f22c10e798a750e1fe630d5d7cdebca
manthan161203 <manthanlp3204@gmail.com>
2023-12-15 13:50:52 +0530

    Schema Ready

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -12,7 +12,7 @@ const appointmentSchema = new mongoose.Schema({
         required: true
     },
     date: {
-        type: Date,
+        type: String,
         required: true
     },
     slot: {
