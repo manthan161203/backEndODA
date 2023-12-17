13093ff1035496e135d3df884c1cf0aa5e91d53f
manthan161203 <manthanlp3204@gmail.com>
2023-12-17 21:42:21 +0530

    Login Controller Done

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -25,37 +25,6 @@ const addOTPToList = async (userName, otp) => {
     }
 };
 
-// Add OTP by userId route handler
-const addOTPByUserId = async (req, res) => {
-    const userId = req.params.userId;
-    const sendMethod = req.params.sendMethod; // 'sms' or 'email'
-
-    try {
-        const otp = generateOTP();
-        
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        await addOTPToList(user.userName, otp);
-
-        if (sendMethod === 'sms') {
-            const phoneNumber = user.phoneNumber;
-            await sendOTPViaSMS(phoneNumber, otp);
-        } else if (sendMethod === 'email') {
-            const email = user.email;
-            await sendOTPViaEmail(email, "Verification Code", "Your verification code is: " + otp);
-        } else {
-            throw new Error('Invalid send method');
-        }
-
-        res.status(200).json({ message: `OTP added to the user's list and sent Via ${sendMethod.toUpperCase()} successfully` });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Failed to add OTP or send Via SMS/Email' });
-    }
-};
-
 // Add OTP by userName route handler
 const addOTPByUserName = async (req, res) => {
     const userName = req.params.userName;
@@ -87,37 +56,33 @@ const addOTPByUserName = async (req, res) => {
     }
 };
 
-const login = async (req, res) => {
-    const { userName, otpCode, password, role } = req.body;
+// Verify OTP and Password by userName route handler
+const verifyOTPByUserName = async (req, res) => {
+    const userName = req.params.userName;
+    const enteredOTP = req.body.otp;
+    const enteredPassword = req.body.password;
 
     try {
-        const user = await User.findOne({ userName, role });
-
+        const user = await User.findOne({ userName });
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        // Verify OTP
-        const otpIndex = user.otp.findIndex(otp => otp.code === otpCode && new Date(otp.expiryTime) > new Date());
+        const otpIndex = user.otp.findIndex(otp => otp.code === enteredOTP && new Date(otp.expiryTime) > new Date());
         if (otpIndex === -1) {
-            return res.status(401).json({ message: 'Invalid OTP or OTP expired' });
+            return res.status(400).json({ message: 'Invalid OTP or OTP expired' });
         }
 
-        // Validate password
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        // Verify Password
+        const isPasswordValid = await bcrypt.compare(enteredPassword, user.password);
         if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Invalid credentials' });
+            return res.status(401).json({ message: 'Invalid password' });
         }
 
-        // Generate JWT token
-        const secretKey = crypto.randomBytes(32).toString('hex');
-        const token = jwt.sign({ userId: user._id, userName: user.userName, role: user.role }, secretKey, { expiresIn: '1h' });
-
-        // Remove used OTP from the user's list
         user.otp.splice(otpIndex, 1);
         await user.save();
 
-        res.status(200).json({ token });
+        res.status(200).json({ message: 'OTP and password verified successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
@@ -125,9 +90,6 @@ const login = async (req, res) => {
 };
 
 module.exports = {
-    login,
-    addOTPByUserId,
     addOTPByUserName,
-    generateOTP,
-    addOTPToList
+    verifyOTPByUserName
 };
