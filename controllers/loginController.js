8c5a5c5f4357df707a86c19bd350004a415b08d0
manthan161203 <manthanlp3204@gmail.com>
2023-12-17 09:08:40 +0530

    Working on OTP login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,43 +4,55 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const sendOTPviaSMS = require('../services/otpTwilio');
 
-const secretKey = crypto.randomBytes(32).toString('hex');
+// Function to generate OTP
+const generateOTP = () => {
+    const otp = crypto.randomBytes(3).toString('hex').toUpperCase(); // Generate a 6-letter OTP
+    return otp;
+};
 
-// sendOTPviaSMS('+917383161203', '1234');
+// Function to add OTP to user's list
+const addOTPToList = async (userName, otp) => {
+    try {
+        const user = await User.findOne({ userName });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.otp.push({ code: otp, expiryTime: new Date(Date.now() + 600000).toISOString() });
+        await user.save();
+    } catch (error) {
+        throw new Error('Failed to add OTP: ' + error.message);
+    }
+};
 
+// Login function
 const login = async (req, res) => {
     const { userName, otpCode, password, role } = req.body;
 
     try {
         const user = await User.findOne({ userName, role });
 
+        // Handle user not found
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        // Check if OTP exists for the user
-        const otpIndex = user.otp.findIndex(
-            otp => otp.code === otpCode && new Date(otp.expiryTime) > new Date()
-        );
-
+        // Verify OTP
+        const otpIndex = user.otp.findIndex(otp => otp.code === otpCode && new Date(otp.expiryTime) > new Date());
         if (otpIndex === -1) {
             return res.status(401).json({ message: 'Invalid OTP or OTP expired' });
         }
 
-        // Continue with password validation only after OTP verification succeeds
+        // Validate password
         const isPasswordValid = await bcrypt.compare(password, user.password);
-
         if (!isPasswordValid) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        const token = jwt.sign(
-            { userId: user._id, userName: user.userName, role: user.role },
-            secretKey,
-            { expiresIn: '1h' }
-        );
+        // Generate JWT token
+        const secretKey = crypto.randomBytes(32).toString('hex');
+        const token = jwt.sign({ userId: user._id, userName: user.userName, role: user.role }, secretKey, { expiresIn: '1h' });
 
-        // Remove the used OTP from the user's OTP list
+        // Remove used OTP from the user's list
         user.otp.splice(otpIndex, 1);
         await user.save();
 
@@ -53,4 +65,6 @@ const login = async (req, res) => {
 
 module.exports = {
     login,
+    addOTPToList,
+    generateOTP
 };
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const loginController = require('../controllers/loginController');
+const User = require('../models/userSchema'); 
 
-router.post('/login', userController.login);
+router.post('/', loginController.login);
+
+router.post('/add-otp/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const otp = loginController.generateOTP();
+        console.log(otp);
+        
+        // Add OTP to the user's list
+        await loginController.addOTPToList(userId, otp);
+
+        // Send OTP via SMS using Twilio
+        const phoneNumber = user.phoneNumber; // Assuming phoneNumber is stored in the user object
+        await sendOTPviaSMS(phoneNumber, otp);
+
+        res.status(200).json({ message: 'OTP added to the user\'s list and sent via SMS successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to add OTP to the user\'s list or send via SMS' });
+    }
+});
+
+
+router.post('/add-otp/:userName', async (req, res) => {
+    const userName = req.params.userName;
+    
+    try {
+        const user = await User.findOne({ userName }); // Use the 'userName' field for the query
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const otp = loginController.generateOTP();
+        
+        // Add OTP to the user's list using userName
+        await loginController.addOTPToList(userName, otp);
+
+        // Send OTP via SMS using Twilio
+        const phoneNumber = user.phoneNumber; // Assuming phoneNumber is stored in the user object
+        await sendOTPviaSMS(phoneNumber, otp); // Send OTP via SMS using Twilio
+        
+        res.status(200).json({ message: 'OTP added to the user\'s list and sent via SMS successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to add OTP to the user\'s list or send via SMS' });
+    }
+});
+
+
 
 module.exports = router;
