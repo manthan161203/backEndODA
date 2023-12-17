8a84660d43a5ed24e9ecd41716d73a065923751b
manthan161203 <manthanlp3204@gmail.com>
2023-12-17 22:25:15 +0530

    Working on register

--- routers/registerRouter.js
+++ routers/registerRouter.js
@@ -1,8 +1,11 @@
+// registerRouter.js
+
 const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
-// Register route
-router.post('/', registerController.register);
+// Register route handlers
+router.post('/submit-info', registerController.submitInfo);
+router.post('/verify-and-register', registerController.verifyAndRegister);
 
 module.exports = router;
