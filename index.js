a17239a6efd9913d1afc3bbe55004b1e7d4bb647
manthan161203 <manthanlp3204@gmail.com>
2023-12-22 13:36:46 +0530

    New commits

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const { PORT, connectDB } = require('./config/database'); // Assuming the connectDB function is defined in the database configuration file
+const { PORT, connectDB } = require('./config/database');
+
+const User = require('./models/userSchema');
+const Patient = require('./models/patientSchema');
+const Appointment = require('./models/appointmentSchema');
+const Doctor = require('./models/unifiedDoctorSchema');
+const SuperAdmin = require('./models/superAdminSchema');
+const Admin = require('./models/adminSchema');
+const Hospital = require('./models/hospitalSchema');
 
 const loginRouter = require('./routers/loginRouter');
 const registerRouter = require('./routers/registerRouter');
-const adminRouter = require('./routers/adminRouter'); // Import your adminRouter
+const adminRouter = require('./routers/adminRouter');
 
 const app = express();
 
@@ -13,10 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.use(cors());
-app.use(fileUpload());
+// app.use(fileUpload());
 
 // Connect to MongoDB
-connectDB(); // Assuming the connectDB function is defined in the database configuration file
+connectDB();
 
 // Routes
 app.use('/login', loginRouter);
