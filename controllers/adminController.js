88b42d50b20ccd0971606bd05154493cb2b28e15
manthan161203 <manthanlp3204@gmail.com>
2024-01-07 13:32:15 +0530

    admin changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,17 +3,20 @@ const Hospital = require("../models/hospitalSchema");
 const User = require("../models/userSchema");
 
 const adminController = {
-    // Get doctors by type, specialization, and hospitalId
+    // Get doctors by type, specialization
     getDoctors: async (req, res) => {
         try {
-            const { doctorType, specialization, hospitalId } = req.body;
-            console.log(req.body)
+            const { doctorType, doctorSpecialization } = req.body;
             const query = {};
-            if (doctorType) query.doctorType = doctorType;
-            if (specialization) query.doctorSpecialization = specialization;
-            if (hospitalId) query.hospitalID = hospitalId;
-            console.log(query)
-            const doctors = await UnifiedDoctor.find(query).populate({path:'hospitalID'}).populate({path:'user',});
+            if (doctorType) {
+                query.doctorType = doctorType;
+            } 
+            if (doctorSpecialization) {
+                query.doctorSpecialization = doctorSpecialization;
+            }
+            console.log(query);
+    
+            const doctors = await UnifiedDoctor.find(query).populate('user');
             res.status(200).json(doctors);
         } catch (error) {
             console.error(error);
@@ -21,72 +24,58 @@ const adminController = {
         }
     },
 
+    // Get doctors by hospitalID
+    getDoctorsByHospitalID : async (req, res) => {
+        try {
+            const { hospitalID } = req.body;
+    
+            const hospital = await Hospital.findOne({ hospitalId: hospitalID });
+                
+            if (!hospital) {
+                return res.status(404).json({ error: 'Hospital not found' });
+            }
+    
+            const doctors = await UnifiedDoctor.find({ hospitalID: hospital._id }).populate('user').populate('hospitalID');
+            res.status(200).json(doctors);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error retrieving doctors by Hospital ID' });
+        }
+    },
+    
     // Get Doctor by UserName
     getDoctorByUserName: async (req, res) => {
         try {
-            const { username } = req.params; // Destructure userName from req.params
-
+            const { username } = req.params;
+    
             if (!username) {
                 return res.status(400).json({ error: 'Username parameter is missing' });
             }
-
+    
             const doctor = await UnifiedDoctor.aggregate([
                 {
                     $lookup: {
                         from: 'users',
                         localField: 'user',
                         foreignField: '_id',
-                        as: 'user'
+                        as: 'userDetails'
                     }
                 },
                 {
                     $match: {
-                        'user.userName': username
+                        $and: [
+                            { 'userDetails.userName': username },
+                            { 'user': { $ne: null } } // Ensures doctor has associated user
+                        ]
                     }
-                },
-                {
-                    $limit: 1 // Limiting to one result, assuming unique usernames
                 }
             ]);
-
-
-            res.status(200).json(doctor);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    },
-
-    // Get hospital by hospitalId and hospitalName
-    getHospital: async (req, res) => {
-        try {
-            const { hospitalId, hospitalName } = req.body;
-
-            if (hospitalId && hospitalName) {
-                const hospitals = await Hospital.find({ hospitalId, hospitalName });
-                if (hospitals.length === 0) {
-                    return res.status(404).json({ error: 'No hospitals found with the provided details' });
-                }
-                return res.status(200).json(hospitals);
+    
+            if (doctor.length === 0) {
+                return res.status(404).json({ error: 'Doctor not found' });
             }
-
-            const query = {};
-
-            if (!hospitalId && !hospitalName) {
-                const hospitals = await Hospital.find();
-                return res.status(200).json(hospitals);
-            }
-
-            if (hospitalId) {
-                query.hospitalId = hospitalId;
-            }
-
-            if (hospitalName) {
-                query.hospitalName = hospitalName;
-            }
-
-            const hospital = await Hospital.find(query);
-            res.status(200).json(hospital);
+    
+            res.status(200).json(doctor[0]);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -132,34 +121,123 @@ const adminController = {
     // Delete a doctor by UserName
     deleteDoctorByUsername: async (req, res) => {
         try {
-            const userName = req.body.username; // Extract the username from the request body
-
-            const doctor = await UnifiedDoctor.findOne({ 'admin.userName': userName }).populate('admin').exec();
-            console.log(doctor);
-
-            // Delete the corresponding doctor from UnifiedDoctor schema
-            const deletedDoctor = await UnifiedDoctor.findOneAndDelete({ 'user.userName': userName }).populate('user');
-
-            if (!deletedDoctor) {
-                console.log(`Doctor or User with username ${userName} not found.`);
+            const { username } = req.body; // Extract the username from the request body
+    
+            const doctor = await UnifiedDoctor.aggregate([
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'user',
+                        foreignField: '_id',
+                        as: 'userDetails'
+                    }
+                },
+                {
+                    $match: {
+                        $or: [
+                            { 'userDetails.userName': username },
+                            { 'admin.userName': username }
+                        ]
+                    }
+                }
+            ]);
+    
+            if (doctor.length === 0) {
+                console.log(`Doctor or User with username ${username} not found.`);
                 return res.status(404).json({ error: 'Doctor or User not found' });
             }
-
-            res.status(204).json({ message: 'Doctor and User have been deleted' });
+    
+            const userIDsToDelete = doctor.map(doc => doc.user); // Assuming 'user' is the user reference field in UnifiedDoctor
+    
+            // Delete the corresponding doctor(s) from UnifiedDoctor schema
+            await UnifiedDoctor.deleteMany({ user: { $in: userIDsToDelete } });
+    
+            res.status(204).json({ message: 'Doctor(s) and associated User(s) have been deleted' });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     },
+    
+
+
+
+
+
 
 
 
     // Create Hospital
     createHospital: async (req, res) => {
         try {
-            const hospital = new Hospital(req.body);
+            const { hospitalId, hospitalName, location, hasAmbulance, ambulances } = req.body;
+    
+            const existingHospital = await Hospital.findOne({ hospitalId });
+    
+            if (existingHospital) {
+                return res.status(400).json({ error: 'Hospital ID already exists' });
+            }
+    
+            const ambulanceDetails = [];
+    
+            if (ambulances && Array.isArray(ambulances)) {
+                ambulances.forEach(ambulance => {
+                    const { plateNumber, model, isAvailable } = ambulance;
+                    ambulanceDetails.push({
+                        plateNumber,
+                        model,
+                        isAvailable
+                    });
+                });
+            }
+    
+            const hospital = new Hospital({
+                hospitalId,
+                hospitalName,
+                location,
+                hasAmbulance,
+                ambulances: ambulanceDetails
+            });
+    
             await hospital.save();
-            res.status(201).json(hospital);
+    
+            res.status(201).json({ message: 'Hospital created successfully', hospital });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+    
+    // Get hospital by hospitalId and hospitalName
+    getHospital: async (req, res) => {
+        try {
+            const { hospitalId, hospitalName } = req.body;
+
+            if (hospitalId && hospitalName) {
+                const hospitals = await Hospital.find({ hospitalId, hospitalName });
+                if (hospitals.length === 0) {
+                    return res.status(404).json({ error: 'No hospitals found with the provided details' });
+                }
+                return res.status(200).json(hospitals);
+            }
+
+            const query = {};
+
+            if (!hospitalId && !hospitalName) {
+                const hospitals = await Hospital.find();
+                return res.status(200).json(hospitals);
+            }
+
+            if (hospitalId) {
+                query.hospitalId = hospitalId;
+            }
+
+            if (hospitalName) {
+                query.hospitalName = hospitalName;
+            }
+
+            const hospital = await Hospital.find(query);
+            res.status(200).json(hospital);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -187,18 +265,21 @@ const adminController = {
     deleteHospitalById: async (req, res) => {
         try {
             const { hospitalId } = req.params;
-            const hospital = await Hospital.findByIdAndDelete({ "hospitalId": hospitalId });
-
+            const hospital = await Hospital.findOneAndDelete({ hospitalId });
+    
             if (!hospital) {
                 return res.status(404).json({ error: 'Hospital not found' });
             }
-
-            res.status(204).json();
+    
+            res.status(201).json({ message: 'Hospital deleted successfully' });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     },
+    
+    
+
 };
 
 module.exports = adminController;
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.post('/getAllDoctors', adminController.getDoctors);
+router.post('/getDoctorsByHospitalID', adminController.getDoctorsByHospitalID); // half
 router.get('/getDoctor/:username', adminController.getDoctorByUserName);
-router.get('/getHospital', adminController.getHospital);
+
 router.delete('/deleteDoctor', adminController.deleteDoctorByUsername);
 router.patch('/updateDoctor/:username', adminController.updateDoctorByUsername);
-router.post('/hospitals', adminController.createHospital);
 router.patch('/hospitals/:hospitalId', adminController.updateHospitalById);
-router.delete('/hospitals/:hospitalId', adminController.deleteHospitalById);
+
+router.post('/createHospital', adminController.createHospital);
+router.post('/getHospital', adminController.getHospital);
+router.delete('/deleteHospital/:hospitalId', adminController.deleteHospitalById);
 
 module.exports = router;
