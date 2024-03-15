5fe38fa125dcbc388ccd99032e881eb8f65b98ec
Tirth Prajapati <tirthprajapati26@gmail.com>
2024-03-15 10:56:05 +0530

    unified doctor router updated

--- routers/unifiedDoctorRouter.js
+++ routers/unifiedDoctorRouter.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 const doctorController = require('../controllers/unifiedDoctorController');
+
 router.put('/updateUserDataDoctor/:userName', adminController.updateUserDataOfDoctorByUserName);
 router.put('/updateDoctorData/:userName', adminController.updateDoctorDataByUserName);
 router.get('/getAppointments/:doctorID', doctorController.getAppointmentsByDoctorID);
+router.get('/getActiveAppointment/:doctorID', doctorController.getActiveAppointmentsByDoctorID);
 router.put('/appointmentAorD/:appointmentID', doctorController.updateAppointmentStatus);
 router.get('/getPatientInfo/:patientID', doctorController.getPatientInfo);
 router.get('/getDoctorsBySpecialization/:specialization', doctorController.getDoctorBySpecialization);
+router.get('/getDoctorsByHospital/:hospitalName', doctorController.getDoctorByHospital);
+router.get('/getDoctorsByType/:doctorType', doctorController.getDoctorByType);
+router.get('/getTodayAppointmentsCount/:doctorID', doctorController.getTodayAppointmentsCount);
+router.get('/getUpComingAppointmentsCount/:doctorID', doctorController.getUpComingAppointmentsCount);
+router.get('/getPendingAppointmentsCount/:doctorID', doctorController.getPendingAppointmentsCount);
+router.get('/getPendingAppointments/:doctorID', doctorController.getPendingAppointmentsByDoctorID);
+router.get('/acceptAppointment/:appointmentID', doctorController.acceptAppointment);
 
 module.exports = router;
