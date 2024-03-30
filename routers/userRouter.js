e57956b8348e142cad6e1cbbfd8fa46e14b34290
manthan161203 <manthanlp3204@gmail.com>
2024-03-30 16:32:30 +0530

    during the changes in user profile for email and phone number error will be visible

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController');
 router.get('/getUserDetails/:userName', userController.getUserDetails);
 router.put('/updateUserDetails/:userName', userController.updateUserDetails);
 router.get('/getAllUsers', userController.getAllUsers);
+router.post('/checkEmail', userController.checkEmail);
+router.post('/checkPhoneNumber', userController.checkPhoneNumber);
 
 module.exports = router;
